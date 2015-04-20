916b115230d697dc11cd35d7daca79e8e114cb46
Raphael Ribeiro Gomes <raphaelribe@gmail.com>
2015-04-20 15:08:35 -0300

    Comitando os arquivos

---
+++ projeto_php01/bootstrap.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <script src="css" rel="stylesheet" type="text/css"></script>
+    <script src="js" rel="stylesheet" type="text/css"></script>
+</head>
+<body>
+<div class="jumbotron">
+    <div class="container">
+        <h1>Fale conosco</h1>
+        <p>escreva uma mensagem para entrar em contato conosco</p>
+    </div>
+
+</div>
+
+</body>
+</html>
